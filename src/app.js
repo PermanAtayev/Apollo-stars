e198689c41c518b39bc02b286b4f88982b329b2b
Hanzallah <hanzallahazimburney@live.com>
2020-03-23 16:41:17 +0300

    Postgres Setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Client } = require('pg');
 const app = express();
 const port = 3000;
 
@@ -7,3 +8,10 @@ app.get('/', function(req, res){
 });
 
 app.listen(port, () => console.log('listening on port ' + port));
+var connectionString = "postgres://dhdhvlgmxspnaa:40d715a84d1d155402c28d586c5e16878c5d956c1d5e6d572759e09a1a4f32f9@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d26qd2m1vpnlfq"
+const client = new Client({
+  connectionString: connectionString,
+  ssl: true,
+});
+
+client.connect();
