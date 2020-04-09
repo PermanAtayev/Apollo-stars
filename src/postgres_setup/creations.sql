5833a85b5c23559051951d6e8b87f3fdf972870b
Hanzallah <hanzallahazimburney@live.com>
2020-04-09 14:21:49 +0300

    Beautify SQL

--- src/postgres_setup/creations.sql
+++ src/postgres_setup/creations.sql
@@ -1,154 +1,154 @@
-create table person (
-  id int primary key,
-  password int,
-  name varchar (20),
-  surname varchar (20),
-  email varchar (20)
+CREATE TABLE person (
+  id INT PRIMARY KEY,
+  password INT,
+  name VARCHAR (20),
+  surname VARCHAR (20),
+  email VARCHAR (20)
 );
-create table phone (
-  id int REFERENCES person (id) ON DELETE CASCADE,
-  phone_number int,
+CREATE TABLE phone (
+  id INT REFERENCES person (id) ON DELETE CASCADE,
+  phone_number INT,
   PRIMARY KEY(id, phone_number)
 );
-create table student (gpa int, tuition_fee int, date_enrolled date) inherits (person);
-create table TA (ta_since date) inherits (student);
-create table instructor (salary int, date_joined date) inherits (person);
-Create table courses (
-  course_id int primary key,
-  name varchar(100),
-  credits int
+CREATE TABLE student (gpa INT, tuition_fee INT, date_enrolled DATE) INHERITS (person);
+CREATE TABLE TA (ta_since date) INHERITS (student);
+CREATE TABLE instructor (salary INT, date_joined DATE) INHERITS (person);
+CREATE TABLE courses (
+  course_id INT PRIMARY KEY,
+  name VARCHAR(100),
+  credits INT
 );
 CREATE TABLE task_list (
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
-  task_desc varchar (100),
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+  task_desc VARCHAR (100),
   PRIMARY KEY (course_id, task_desc)
 );
-Create table prereq (
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
-  prereq_id int REFERENCES courses(course_id) ON DELETE CASCADE,
+CREATE TABLE prereq (
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+  prereq_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
   PRIMARY KEY (course_id, prereq_id)
 );
-create table courses_taken (
-  student_id int REFERENCES person(id) ON DELETE CASCADE,
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
+CREATE TABLE courses_taken (
+  student_id INT REFERENCES person(id) ON DELETE CASCADE,
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
   PRIMARY KEY (course_id, student_id)
 );
-create table courses_reg (
-  student_id int REFERENCES person(id) ON DELETE CASCADE,
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
-  sec_id int,
+CREATE TABLE courses_reg (
+  student_id INT REFERENCES person(id) ON DELETE CASCADE,
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+  sec_id INT,
   PRIMARY KEY (course_id, student_id, sec_id)
 );
-create table take_exam (
-  student_id int REFERENCES person(id) ON DELETE CASCADE,
-  exam_id int,
-  grade int,
-  primary key (student_id, exam_id)
+CREATE TABLE take_exam (
+  student_id INT REFERENCES person(id) ON DELETE CASCADE,
+  exam_id INT,
+  grade INT,
+  PRIMARY KEY (student_id, exam_id)
 );
-create table take_assignment (
-  student_id int REFERENCES person(id) ON DELETE CASCADE,
-  assignment_id int,
-  grade int,
-  primary key (student_id, assignment_id)
+CREATE TABLE take_assignment (
+  student_id INT REFERENCES person(id) ON DELETE CASCADE,
+  assignment_id INT,
+  grade INT,
+  PRIMARY KEY (student_id, assignment_id)
 );
-create table course_exam (
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
-  exam_id int,
+CREATE TABLE course_exam (
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+  exam_id INT,
   PRIMARY KEY (course_id, exam_id)
 );
-create table course_assignment (
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
-  assignment_id int,
+CREATE TABLE course_assignment (
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+  assignment_id INT,
   PRIMARY KEY (course_id, assignment_id)
 );
-create table section (
-  section_id varchar (6),
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
-  building_no int,
-  room_no int,
-  primary key (section_id, course_id)
+CREATE TABLE section (
+  section_id VARCHAR (6),
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+  building_no INT,
+  room_no INT,
+  PRIMARY KEY (section_id, course_id)
 );
-create table teaches (
-  instructor_id int REFERENCES person(id) ON DELETE CASCADE,
-  section_id varchar (6),
-  course_id int,
-  primary key (instructor_id, section_id),
+CREATE TABLE teaches (
+  instructor_id INT REFERENCES person(id) ON DELETE CASCADE,
+  section_id VARCHAR (6),
+  course_id INT,
+  PRIMARY KEY (instructor_id, section_id),
   FOREIGN KEY (section_id, course_id) REFERENCES section(section_id, course_id) ON DELETE CASCADE
 );
-create table sec_course (
-  section_id varchar(6),
-  course_id int,
-  primary key (section_id, course_id),
+CREATE TABLE sec_course (
+  section_id VARCHAR(6),
+  course_id INT,
+  PRIMARY KEY (section_id, course_id),
   FOREIGN KEY (section_id, course_id) REFERENCES section(section_id, course_id) ON DELETE CASCADE
 );
-create table student_sec (
-  student_id int REFERENCES person(id) ON DELETE CASCADE,
-  section_id varchar (6),
-  course_id int,
-  grade int,
-  primary key (section_id, student_id),
+CREATE TABLE student_sec (
+  student_id INT REFERENCES person(id) ON DELETE CASCADE,
+  section_id VARCHAR (6),
+  course_id INT,
+  grade INT,
+  PRIMARY KEY (section_id, student_id),
   FOREIGN KEY (section_id, course_id) REFERENCES section(section_id, course_id) ON DELETE CASCADE
 );
-create table assists (
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
-  ta_id int REFERENCES person(id) ON DELETE CASCADE,
-  primary key (course_id, ta_id)
+CREATE TABLE assists (
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+  ta_id INT REFERENCES person(id) ON DELETE CASCADE,
+  PRIMARY KEY (course_id, ta_id)
 );
-create table research_group (
-  group_id int,
-  instructor_id int REFERENCES person(id) ON DELETE CASCADE,
-  research_grant int,
-  research_topic varchar (280),
+CREATE TABLE research_group (
+  group_id INT,
+  instructor_id INT REFERENCES person(id) ON DELETE CASCADE,
+  research_grant INT,
+  research_topic VARCHAR (280),
   PRIMARY KEY (group_id)
 );
-create table study_group (
-  group_id int PRIMARY KEY,
-  mod_id int REFERENCES person(id) ON DELETE CASCADE,
-  group_name varchar (100),
-  purpose varchar (100)
+CREATE TABLE study_group (
+  group_id INT PRIMARY KEY,
+  mod_id INT REFERENCES person(id) ON DELETE CASCADE,
+  group_name VARCHAR (100),
+  purpose VARCHAR (100)
 );
-create table career (
-  career_id int PRIMARY KEY,
-  career_desc varchar (100),
-  career_type varchar (100),
-  salary int
+CREATE TABLE career (
+  career_id INT PRIMARY KEY,
+  career_desc VARCHAR (100),
+  career_type VARCHAR (100),
+  salary INT
 );
-create table classroom (
-  building_no int,
-  room_no int,
-  capacity int,
+CREATE TABLE classroom (
+  building_no INT,
+  room_no INT,
+  capacity INT,
   PRIMARY KEY (building_no, room_no)
 );
-create table department (
-  department_name varchar (10) PRIMARY KEY,
-  building_no int,
-  budget int
+CREATE TABLE department (
+  department_name VARCHAR (10) PRIMARY KEY,
+  building_no INT,
+  budget INT
 );
-create table assignemnt (
-  assignment_id int PRIMARY KEY,
-  assignment_name varchar(10),
+CREATE TABLE assignemnt (
+  assignment_id INT PRIMARY KEY,
+  assignment_name VARCHAR(10),
   due_date DATE
 );
-create table exam (
-  exam_id int PRIMARY KEY,
-  start_time timestamp,
-  end_time timestamp
+CREATE TABLE exam (
+  exam_id INT PRIMARY KEY,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP
 );
-create table joins_research (
-  group_id int REFERENCES research_group (group_id),
-  student_id int REFERENCES person(id) ON DELETE CASCADE,
-  primary key (group_id, student_id)
+CREATE TABLE joins_research (
+  group_id INT REFERENCES research_group (group_id),
+  student_id INT REFERENCES person(id) ON DELETE CASCADE,
+  PRIMARY KEY (group_id, student_id)
 );
-create table participates (
-  group_id int REFERENCES study_group (group_id),
-  student_id int REFERENCES person(id) ON DELETE CASCADE,
-  primary key (group_id, student_id)
+CREATE TABLE participates (
+  group_id INT REFERENCES study_group (group_id),
+  student_id INT REFERENCES person(id) ON DELETE CASCADE,
+  PRIMARY KEY (group_id, student_id)
 );
-create table auth_ta (
-  instructor_id int REFERENCES person(id) ON DELETE CASCADE,
-  ta_id int REFERENCES person(id) ON DELETE CASCADE,
-  task varchar(100),
-  course_id int REFERENCES courses(course_id) ON DELETE CASCADE,
-  is_done boolean,
+CREATE TABLE auth_ta (
+  instructor_id INT REFERENCES person(id) ON DELETE CASCADE,
+  ta_id INT REFERENCES person(id) ON DELETE CASCADE,
+  task VARCHAR(100),
+  course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+  is_done BOOLEAN,
   PRIMARY KEY (instructor_id, ta_id)
 );
