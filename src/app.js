06e313627779e1a5d3df1a84daa0bab50636bb5c
Hanzallah <hanzallahazimburney@live.com>
2020-05-18 15:18:20 +0300

    API Student + Instructor Updated

--- src/app.js
+++ src/app.js
@@ -191,7 +191,7 @@ passport.deserializeUser(function(user, done) {
   
 // ---------------------------------- User Routes -------------------------------------------- //
 // list all courses that a student can register for
-app.get('/student/:id/regCourses', (req,res)=>{
+app.get('/student/:id/regCourseApply', (req,res)=>{
   student_id = req.params.id;
   q = `SELECT
   DISTINCT course_name
@@ -220,23 +220,23 @@ app.get('/student/:id/regCourses', (req,res)=>{
     SELECT
       course_id
     FROM Courses_Taken
-  );`
+  );`;
 
-  client.query(q,[id, req.body.email, password, req.body.fname, req.body.surname, req.body.phone_no], (err, result)=>{
+  client.query(q, (err, result)=>{
     if (err){
       console.log(err);
     }
     else{
-      res.redirect('/student/:id');
+      res.send(result);
     }
   });
 });
 
 // select and register a course for a student
-  // the student picks an available section from a course they can take.
+// the student picks an available section from a course they can take.
 app.post('/student/:id/register', (req,res)=>{
  q = `INSERT INTO Courses_Registered
- VALUES($1, $2, $3);`
+ VALUES($1, $2, $3);`;
  client.query(q,[req.params.id, req.body.course_id, req.body.section_id], (err, result)=>{
     if (err){
       console.log(err);
@@ -247,7 +247,332 @@ app.post('/student/:id/register', (req,res)=>{
   });
 });
 
+// list all registered courses of a student
+app.get('/student/:id/regCourseView', (req,res)=>{
+  q = `SELECT
+    course_name
+  FROM Course,
+    Courses_Registered
+  WHERE
+    Course.id = Courses_Registered.course_id;`;
+  client.query(q, (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+// select a course and display grades for that course
+app.get('/student/:id/regCourseView', (req,res)=>{
+  q = `Select
+  grade
+  From Take_Exam,
+  Course_Exam
+  WHERE
+  Take_Exam.exam_id IN (
+    SELECT
+      exam_id
+    FROM Course_Exam
+  )
+  AND Course_Exam.course_id = $1;
+  Select
+  grade
+  From Take_Assignment,
+  Course_Assignment
+  WHERE
+  Take_Assignment.assignment_id IN (
+    SELECT
+      assignment_id
+    FROM Course_Assignment
+  )
+  AND Course_Assignment.course_id = $1;`;
+
+  client.query(q,[req.body.course_id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+
+});
+
+// display and update student details such as email and password
+app.get('/student/:id/details/display', (req,res)=>{
+  q = `SELECT * FROM Person,Student WHERE Person.id = $1;`;
+  client.query(q, [req.params.id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+app.post('/student/:id/details/update', (req,res)=>{
+  q = `UPDATE Person SET email = $1, password = $2 WHERE id = $3;`;
+  var password = await bcrypt.hash(req.body.password, 5);
+  client.query(q, [req.body.email, password, req.params.id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log('Details Updated');
+    }
+  });
+});
+
+// list all exams
+app.get('/student/:id/exams/display', (req,res)=>{
+  q = `SELECT
+  DISTINCT exam_name
+FROM Student,
+  Take_Exam,
+  Exam
+WHERE
+  Student.id = $1
+  AND Take_Exam.student_id = Student.id
+  AND Take_Exam.exam_id = Exam.id;`;
+  client.query(q, [req.params.id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+// list all assignments
+app.get('/student/:id/assignments/display', (req,res)=>{
+  q = `SELECT
+  DISTINCT assignment_name
+FROM Student,
+  Take_Assignment,
+  Assignment
+WHERE
+  Student.id = $1
+  AND Take_Assignment.student_id = Student.id
+  AND Take_Assignment.assignment_id = Assignment.id;`;
+  client.query(q, [req.params.id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+// join a research group
+// if application has been approved
+app.post('/student/:id/research/apply', (req, res)=>{
+  q = `INSERT INTO Joins_Research Values($1, $2);`;
+  var accept = Math.random();
+  if (accept){
+    client.query(q, [req.body.group_id, req.params.id], (err,result)=>{
+      if (err){
+        console.log(err);
+      }
+      else{
+        console.log("Reasearch application approved!");
+      }
+    });
+  }
+  else{
+    console.log("Application to join the group has been rejected!");
+  }
+  
+});
+
+// create study group
+app.post('/student/:id/research/apply', (req, res)=>{
+  q = `INSERT INTO Study_Group Values($1, $2, $3, $4);`;
+  client.query(q, [req.body.group_id, req.params.id, req.body.group_name, req.body.purpose], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log('Study group created!');
+    }
+  });
+});
+
+
+// join study group
+// if application has been approved
+app.post('/student/:id/study/apply', (req, res)=>{
+  q = `INSERT INTO Participates Values($1, $2);`;
+  var accept = Math.random();
+  if (accept){
+    client.query(q, [req.body.group_id, req.params.id], (err,result)=>{
+      if (err){
+        console.log(err);
+      }
+      else{
+        console.log("Reasearch application approved!");
+      }
+    });
+  }
+  else{
+    console.log("Application to join the group has been rejected!");
+  }
+  
+});
+
+// list study groups
+app.get('/student/:id/study_group/', (req,res)=>{
+  q = `SELECT DISTINCT group_nam FROM Student, Participates, Study_Group
+  WHERE Student.id = $1 AND Participates.student_id = Student.id AND Participates.group_id = Study_Group.id;`;
+  client.query(q, [req.params.id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+//view available career posts
+app.get('/student/:id/careers/display', (req,res)=>{
+  q = `Select * From Careers `;
+  client.query(q, (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+// assess an instructor
+app.post('/student/:id/assess/instructor', (req,res)=>{
+  q = `INSERT INTO Reviews VALUES($1, $2, $3, $4);`;
+  client.query(q, [req.body.instructor_id, req.params.id, req.body.points, req.body.comments], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log("Instructor Assessment Submitted.");
+    }
+  });
+});
 // ---------------------------------- Instructor Routes -------------------------------------- //
+// list all the courses of the instructor
+app.get('/instructor/:id/courses/display', (req,res)=>{
+  q = `SELECT DISTINCT course_name FROM Course, Section, Teaches
+  WHERE Teaches.instructor_id = $1 AND Teaches.section_id = Section.section_id AND Course.course_id = Section.course_id;`;
+  client.query(q, [req.params.id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+// list TA's of a course
+app.get('/instructor/:id/course/TA/display', (req,res)=>{
+  q = `SELECT DISTINCT name FROM Course, TA, Assists WHERE Assists.course_id = $1 AND Assists.ta_id = TA.ta_id;`;
+  client.query(q, [req.body.course_id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+// list possible tasks for a TA for a specific course
+app.get('/instructor/:id/course/TA/tasks/display', (req,res)=>{
+  q = `SELECT task_desc FROM Task_List, Teaches WHERE 
+  Teaches.instructor_id = $1 AND Teaches.course_id = $2 AND Task_List.course_id = $2;`;
+  client.query(q, [req.params.id, req.body.course_id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+// list all exams
+app.get('/instructor/:id/exams/display', (req,res)=>{
+  q = `SELECT DISTINCT exam_name FROM Instructor, Schedules_Exam, Exam WHERE
+  Instructor.id = $1 AND Schedules_Exam.instructor_id = Instructor.id AND Schedules_Exam.exam_id = Exam.id;`;
+  client.query(q, [req.params.id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+// authorize TA's for doing specific tasks such as submitting hw grades, attendance etc.
+app.post('/instructor/:id/authorize/TA', (req,res)=>{
+  q = `INSERT INTO Auth_TA VALUES($1, $2, $3, $4, FALSE);`;
+  client.query(q, [req.params.id, req.body.ta_id, req.body.task_desc, req.body.course_id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log("TA Authorized.");
+    }
+  });
+});
+
+
+// select a course and submit course grade for each student registered in the course
+// the section_id is made up of the coursename and an in -- eg OOP-01
+// select a course then select a section then select a student
+app.get('/instructor/:id/select/course', (req,res)=>{
+  q = `SELECT section_id FROM Sec_Course WHERE course_id = $1;`;
+  client.query(q, [req.body.course_id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+app.get('/instructor/:id/select/course/section', (req,res)=>{
+  q = `SELECT student_id FROM Student_Sec WHERE section_id = $1;`;
+  client.query(q, [req.body.section_id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+app.post('/instructor/:id/authorize/TA', (req,res)=>{
+  q = `UPDATE Student_Sec SET grade = $1 WHERE section_id = $2 AND student_id = $3;`;
+  client.query(q, [req.body.grade, req.body.section_id, req.body.student_id], (err,result)=>{
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log("Grade Submitted.");
+    }
+  });
+});
+
 
 // AHD Starts Here --------------------------------------------------------------------------- // 
 
