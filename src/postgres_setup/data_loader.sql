e429979c142e13e21b35bb257094d070694aa7f2
Hanzallah <hanzallahazimburney@live.com>
2020-05-20 13:59:04 +0300

    Update data_loader.sql

--- src/postgres_setup/data_loader.sql
+++ src/postgres_setup/data_loader.sql
@@ -9,37 +9,37 @@
 -- INSERT INTO Department VALUES('SOC', 9, 1000000);
 -- INSERT INTO Department VALUES('ARCH', 10, 9000000);
 
-INSERT INTO Student VALUES(1217013, 'password1', 'Jimmy', 'Williams', 'Williams@harvard.edu',93029321, 'CS', 0, 10000,'2020-01-20');
-INSERT INTO Student VALUES(1217014, 'password1', 'John', 'Johsnon', 'Johsnon@harvard.edu',93029321, 'MBG', 0, 10000,'2020-01-20');
-INSERT INTO Student VALUES(1217015, 'password1', 'James', 'Tolstoy', 'Tolstoy@harvard.edu',93029321, 'MATH', 0, 231320,'2020-01-20');
-INSERT INTO Student VALUES(1217016, 'password1', 'Ben', 'Colbert', 'Colbert@harvard.edu',93029321, 'CS', 0, 42340,'2020-01-20');
-INSERT INTO Student VALUES(1217017, 'password1', 'Alice', 'Kimmel', 'Kimmel@harvard.edu',93029321, 'CS', 0, 6540,'2020-01-20');
-INSERT INTO Student VALUES(1217018, 'password1', 'May', 'Fallon', 'Fallon@harvard.edu',93029321, 'CS', 0, 53420,'2020-01-20');
-INSERT INTO Student VALUES(1217019, 'password1', 'Amy', 'Westwood', 'Westwood@harvard.edu',93029321, 'MBG', 0, 53242,'2020-01-20');
-INSERT INTO Student VALUES(1217021, 'password1', 'Cindy', 'Festerburg', 'Festerburg@harvard.edu',93029321, 'MATH', 0, 5324254,'2020-01-20');
-INSERT INTO Student VALUES(1217022, 'password1', 'Bernard', 'Beerwood', 'Beerwood@harvard.edu',93029321, 'CHEM', 0, 43234,'2020-01-20');
-INSERT INTO Student VALUES(1217023, 'password1', 'Tim', 'Allen', 'Allen@harvard.edu',93029321, 'PSYC', 0, 532442,'2020-01-20');
-INSERT INTO Student VALUES(1217024, 'password1', 'Steve', 'Gunn', 'Gunn@harvard.edu',93029321, 'SOC', 0, 643454,'2020-01-20');
-INSERT INTO Student VALUES(1217025, 'password1', 'Stephen', 'Forester', 'Forester@harvard.edu',93029321, 'ARCH', 0, 6434540,'2020-01-20');
-INSERT INTO Student VALUES(1217026, 'password1', 'Peter', 'Parker', 'Parker@harvard.edu',93029321, 'ME', 0, 6433,'2020-01-20');
-INSERT INTO Student VALUES(1217027, 'password1', 'Sara', 'Jackie', 'Jackie@harvard.edu',93029321, 'EE', 0, 0,'2020-01-20');
-INSERT INTO Student VALUES(1217028, 'password1', 'Mary', 'Selenium', 'Selenium@harvard.edu',93029321, 'EE', 0, 43,'2020-01-20');
-INSERT INTO Student VALUES(1217029, 'password1', 'David', 'Galvin', 'Galvin@harvard.edu',93029321, 'EE', 0, 43423,'2020-01-20');
-INSERT INTO Student VALUES(1217030, 'password1', 'Boris', 'Boron', 'Boron@harvard.edu',93029321, 'MATH', 0, 4344,'2020-01-20');
-INSERT INTO Student VALUES(1217031, 'password1', 'Stephanie', 'Helium', 'Helium@harvard.edu',93029321, 'EE', 0, 899,'2020-01-20');
-INSERT INTO Student VALUES(1217032, 'password1', 'Aron', 'Einstenium', 'Einstenium@harvard.edu',93029321, 'ME', 0, 899987,'2020-01-20');
-INSERT INTO Student VALUES(1217033, 'password1', 'Clara', 'Greenwood', 'Greenwood@harvard.edu',93029321, 'CS', 0, 89,'2020-01-20');
+-- INSERT INTO Student VALUES(1217013, 'password1', 'Jimmy', 'Williams', 'Williams@harvard.edu',93029321, 'CS', 0, 10000,'2020-01-20');
+-- INSERT INTO Student VALUES(1217014, 'password1', 'John', 'Johsnon', 'Johsnon@harvard.edu',93029321, 'MBG', 0, 10000,'2020-01-20');
+-- INSERT INTO Student VALUES(1217015, 'password1', 'James', 'Tolstoy', 'Tolstoy@harvard.edu',93029321, 'MATH', 0, 231320,'2020-01-20');
+-- INSERT INTO Student VALUES(1217016, 'password1', 'Ben', 'Colbert', 'Colbert@harvard.edu',93029321, 'CS', 0, 42340,'2020-01-20');
+-- INSERT INTO Student VALUES(1217017, 'password1', 'Alice', 'Kimmel', 'Kimmel@harvard.edu',93029321, 'CS', 0, 6540,'2020-01-20');
+-- INSERT INTO Student VALUES(1217018, 'password1', 'May', 'Fallon', 'Fallon@harvard.edu',93029321, 'CS', 0, 53420,'2020-01-20');
+-- INSERT INTO Student VALUES(1217019, 'password1', 'Amy', 'Westwood', 'Westwood@harvard.edu',93029321, 'MBG', 0, 53242,'2020-01-20');
+-- INSERT INTO Student VALUES(1217021, 'password1', 'Cindy', 'Festerburg', 'Festerburg@harvard.edu',93029321, 'MATH', 0, 5324254,'2020-01-20');
+-- INSERT INTO Student VALUES(1217022, 'password1', 'Bernard', 'Beerwood', 'Beerwood@harvard.edu',93029321, 'CHEM', 0, 43234,'2020-01-20');
+-- INSERT INTO Student VALUES(1217023, 'password1', 'Tim', 'Allen', 'Allen@harvard.edu',93029321, 'PSYC', 0, 532442,'2020-01-20');
+-- INSERT INTO Student VALUES(1217024, 'password1', 'Steve', 'Gunn', 'Gunn@harvard.edu',93029321, 'SOC', 0, 643454,'2020-01-20');
+-- INSERT INTO Student VALUES(1217025, 'password1', 'Stephen', 'Forester', 'Forester@harvard.edu',93029321, 'ARCH', 0, 6434540,'2020-01-20');
+-- INSERT INTO Student VALUES(1217026, 'password1', 'Peter', 'Parker', 'Parker@harvard.edu',93029321, 'ME', 0, 6433,'2020-01-20');
+-- INSERT INTO Student VALUES(1217027, 'password1', 'Sara', 'Jackie', 'Jackie@harvard.edu',93029321, 'EE', 0, 0,'2020-01-20');
+-- INSERT INTO Student VALUES(1217028, 'password1', 'Mary', 'Selenium', 'Selenium@harvard.edu',93029321, 'EE', 0, 43,'2020-01-20');
+-- INSERT INTO Student VALUES(1217029, 'password1', 'David', 'Galvin', 'Galvin@harvard.edu',93029321, 'EE', 0, 43423,'2020-01-20');
+-- INSERT INTO Student VALUES(1217030, 'password1', 'Boris', 'Boron', 'Boron@harvard.edu',93029321, 'MATH', 0, 4344,'2020-01-20');
+-- INSERT INTO Student VALUES(1217031, 'password1', 'Stephanie', 'Helium', 'Helium@harvard.edu',93029321, 'EE', 0, 899,'2020-01-20');
+-- INSERT INTO Student VALUES(1217032, 'password1', 'Aron', 'Einstenium', 'Einstenium@harvard.edu',93029321, 'ME', 0, 899987,'2020-01-20');
+-- INSERT INTO Student VALUES(1217033, 'password1', 'Clara', 'Greenwood', 'Greenwood@harvard.edu',93029321, 'CS', 0, 89,'2020-01-20');
 
-INSERT INTO Instructor VALUES(1217034, 'password1', 'Mario', 'Greenwood', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217035, 'password1', 'Petey', 'Cruiser', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217036, 'password1', 'Anna', 'Sthesia', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217037, 'password1', 'Paul', 'Molive', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217038, 'password1', 'Anna', 'Mull', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217039, 'password1', 'Gail', 'Forcewind', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217040, 'password1', 'Paige', 'Turner', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217041, 'password1', 'Bob', 'Frapples', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217042, 'password1', 'Walter', 'Melon', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
-INSERT INTO Instructor VALUES(1217043, 'password1', 'Nick', 'Bocker', 'atayev@harvard.edu', 123, 'CS', 100, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217034, 'password1', 'Mario', 'Greenwood', 'Mario@harvard.edu', 123, 'CS', 100, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217035, 'password1', 'Petey', 'Cruiser', 'Petey@harvard.edu', 123, 'CS', 200, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217036, 'password1', 'Anna', 'Sthesia', 'Anna@harvard.edu', 123, 'CS', 300, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217037, 'password1', 'Paul', 'Molive', 'Paul@harvard.edu', 123, 'CS', 400, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217038, 'password1', 'Anna', 'Mull', 'Mull@harvard.edu', 123, 'CS', 500, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217039, 'password1', 'Gail', 'Forcewind', 'Forcewind@harvard.edu', 123, 'CS', 600, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217040, 'password1', 'Paige', 'Turner', 'Turner@harvard.edu', 123, 'CS', 700, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217041, 'password1', 'Bob', 'Frapples', 'Frapples@harvard.edu', 123, 'CS', 800, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217042, 'password1', 'Walter', 'Melon', 'Melon@harvard.edu', 123, 'CS', 900, '2020-01-20');
+-- INSERT INTO Instructor VALUES(2217043, 'password1', 'Nick', 'Bocker', 'Bocker@harvard.edu', 123, 'CS', 10000, '2020-01-20');
 
 
 
