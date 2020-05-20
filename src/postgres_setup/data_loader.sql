874dc8a6f693f5255fea3c93a62e3ffa12e46973
Hanzallah <hanzallahazimburney@live.com>
2020-05-20 13:59:51 +0300

    Update data_loader.sql

--- src/postgres_setup/data_loader.sql
+++ src/postgres_setup/data_loader.sql
@@ -42,14 +42,9 @@
 -- INSERT INTO Instructor VALUES(2217043, 'password1', 'Nick', 'Bocker', 'Bocker@harvard.edu', 123, 'CS', 10000, '2020-01-20');
 
 
-
--- INSERT INTO Instructor VALUES();
--- INSERT INTO TA VALUES();
-
 -- Courses
 -- Sections
 -- Prereq
 -- Classroom
--- Department
 -- Assists
 -- Career
