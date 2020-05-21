a2ad93a76fd14b5c3f4725651f6214c6a5e16a42
PermanAtayev <perman.atayev17@gmail.com>
2020-05-21 11:23:50 +0300

    cors policy is allowed now

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ var bodyParser = require('body-parser');
 // create express app
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.use(express.json())
 
 let server = require('http').Server(app);
