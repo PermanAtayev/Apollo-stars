c0f83302183f0cbd70188bef6e4e560589a5b15d
Balaj Saleem <balajsaleem@live.com>
2020-05-21 12:41:41 +0500

    some commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,4 @@
-<template>
+ <template>
   <v-app class="primary">
     <v-content>
       <v-container class="fill-height" fluid>
@@ -10,13 +10,15 @@
                   <v-row>
                     <v-col cols="12" md="8">
                       <v-card-text class="mt12">
-                        <h2 class="text-center display-1 teal--text text--accent-3">
-                         Welcome back to ApolloStars!</h2>
+                        <h2
+                          class="text-center display-1 teal--text text--accent-3"
+                        >Welcome back to ApolloStars!</h2>
                         <h4 class="text-center mlt-4">Please Sign In with your credentials</h4>
                         <v-form>
                           <v-text-field
-                            label="Email"
-                            name="Email"
+                            label="id"
+                            name="id"
+                            v-model="id"
                             prepend-icon="email"
                             type="text"
                             color="teal accent-3"
@@ -24,6 +26,7 @@
                           <v-text-field
                             label="Password"
                             name="Passowrd"
+                            v-model="password"
                             prepend-icon="lock"
                             type="password"
                             color="teal accent-3"
@@ -38,21 +41,21 @@
                             <v-radio label="Teaching Assistant" :value="ta" color="teal"></v-radio>
                           </v-radio-group>
                         </v-card-actions>
-                        </v-layout>
-          
+                      </v-layout>
+
                       <div class="text-center mt-3">
-                        <v-btn class="mt-3" rounded color="teal accent-3" dark large>Sign In</v-btn>
+                        <v-btn class="mt-3" rounded color="teal accent-3" dark large @click="login()">Sign In</v-btn>
                       </div>
                     </v-col>
                     <v-col cols="12" md="4" class="teal accent-3">
                       <v-card-text class="white--text mt-12">
-                          <h1 class="text-center display-1">Not registered?</h1>
-                          <h5 class="text-center mt-4">Provide your details, and sign up!</h5>
+                        <h1 class="text-center display-1">Not registered?</h1>
+                        <h5 class="text-center mt-4">Provide your details, and sign up!</h5>
                       </v-card-text>
                       <div class="text-center">
-                        <v-btn rounded outlined="" dark @click="step++">Sign Up</v-btn>
+                        <v-btn rounded outlined dark @click="step++">Sign Up</v-btn>
                         <h5 class="white--text text-center pa-4">View ApolloStars Reports!</h5>
-                        <v-btn rounded outlined="" dark router to="/reports" >Reports</v-btn>
+                        <v-btn rounded outlined dark router to="/reports">Reports</v-btn>
                       </div>
                     </v-col>
                   </v-row>
@@ -64,7 +67,7 @@
                         <h1 class="text-center display-1">Already registered?</h1>
                         <h5 class="text-center mt-6">Use your given credentials to log in</h5>
                       </v-card-text>
-                      <div class="text-center"> 
+                      <div class="text-center">
                         <v-btn class="mt-3" rounded outlined dark @click="step--">Sign In</v-btn>
                       </div>
                     </v-col>
@@ -74,40 +77,41 @@
                         <h4 class="text-center mt-4">Enter the following details please</h4>
                         <v-from>
                           <v-text-field
-                          label="Full Name"
-                          name="Full Name"
-                          prepend-icon="person"
-                          type="text"
-                          color="teal accent-3"
+                            label="Full Name"
+                            name="Full Name"
+                            prepend-icon="person"
+                            type="text"
+                            color="teal accent-3"
                           ></v-text-field>
                           <v-text-field
-                          label="Email"
-                          name="Email"
-                          prepend-icon="email"
-                          type="text"
-                          color="teal accent-3"
+                            label="ID"
+                            name="ID"
+                            
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
                           ></v-text-field>
                           <v-text-field
-                          label="Password"
-                          name="Password"
-                          prepend-icon="lock"
-                          type="password"
-                          color="teal accent-3"
+                            label="Password"
+                            name="Password"
+
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
                           ></v-text-field>
                         </v-from>
                         <v-layout justify-center>
-                        <v-card-actions>
-                          <v-radio-group row v-model="role" class="justify-center">
-                            <v-radio label="Teacher" :value="teacher" color="teal"></v-radio>
-                            <v-radio label="Student" :value="student" color="teal"></v-radio>
-                            <v-radio label="Teaching Assistant" :value="ta" color="teal"></v-radio>
-                          </v-radio-group>
-                        </v-card-actions>
+                          <v-card-actions>
+                            <v-radio-group row v-model="role" class="justify-center">
+                              <v-radio label="Teacher" :value="teacher" color="teal"></v-radio>
+                              <v-radio label="Student" :value="student" color="teal"></v-radio>
+                              <v-radio label="Teaching Assistant" :value="ta" color="teal"></v-radio>
+                            </v-radio-group>
+                          </v-card-actions>
                         </v-layout>
                         <div class="text-center mt-3">
                           <v-btn rounded color="teal accent-3" dark large>Sign UP</v-btn>
                         </div>
-
                       </v-card-text>
                     </v-col>
                   </v-row>
@@ -124,10 +128,36 @@
 export default {
   data: () => ({
     step: 1,
-    role: ""
+    role: "",
+    id: "",
+    password: ""
   }),
   props: {
     source: String
+  },
+  methods: {
+    login: async function() {
+      console.log("logging in with id: " + parseInt(this.id), " pass:" +  this.password);
+      let data = { id: parseInt(this.id), password: this.password };
+
+      const settings = {
+        method: "get",
+        headers: {
+          "content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      };
+
+      const res = await fetch("localhost:8079/login", settings)
+        .then(response => response.json())
+        .then(json => {
+          console.log(res);
+          return console.log(json);
+        })
+        .catch(e => {
+          return e;
+        });
+    }
   }
 };
 </script>
